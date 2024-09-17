2ac0989fe59bd877acda13597403aea1b0adadbc
simsonnad <simson.nad@gmail.com>
2024-09-17 09:44:39 +0530

    up

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+erraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.67.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "sim-bucket" {
+    bucket = "simson-bucket-devops
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+}
+}
